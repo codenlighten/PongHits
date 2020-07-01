6a0df2847a2c1deaa0bd07e6bb1c4d3bc0dacdbe
[dylanjwu] <dylanjwu@gmail.com>
2020-07-01 10:39:24 -0400

    added source link to script

--- puppScript.js
+++ puppScript.js
@@ -1,31 +1,33 @@
+//copied from: https://thecodebarbarian.com/testing-vue-apps-with-puppeteer-and-mocha.html
+
 const puppeteer = require('puppeteer');
 
 run().then(() => console.log('Done')).catch(error => console.log(error));
 
 async function run() {
-  // Create a new Chromium browser. `slowMo` means Chrome waits for 250ms
-  // between operations, so you can see what's happening.
-  const browser = await puppeteer.launch({ headless: false, slowMo: 250 });
-  const page = await browser.newPage();
-  await page.goto('https://google.com');
+    // Create a new Chromium browser. `slowMo` means Chrome waits for 250ms
+    // between operations, so you can see what's happening.
+    const browser = await puppeteer.launch({ headless: false, slowMo: 250 });
+    const page = await browser.newPage();
+    await page.goto('https://google.com');
 
-  // Type "Vue" into the search bar
-  await page.evaluate(() => {
-    document.querySelector('input[name="q"]').value = 'Bob Dylan';
-  });
+    // Type "Vue" into the search bar
+    await page.evaluate(() => {
+        document.querySelector('input[name="q"]').value = 'Bob Dylan';
+    });
 
-  // Click on the "Google Search" button and wait for the page to load
-  const waitForLoad = new Promise(resolve => page.on('load', () => resolve()));
-  await page.evaluate(() => {
-    document.querySelector('input[value="Google Search"]').click();
-  });
-  await waitForLoad;
+    // Click on the "Google Search" button and wait for the page to load
+    const waitForLoad = new Promise(resolve => page.on('load', () => resolve()));
+    await page.evaluate(() => {
+        document.querySelector('input[value="Google Search"]').click();
+    });
+    await waitForLoad;
 
-  // Get the contents of the first link in the search results. Should be
-  // "https://vuejs.org/"
-	
-  const firstLink = await page.evaluate(() => document.querySelector('#search link').getAttribute('href'));
-  console.log(firstLink);
+    // Get the contents of the first link in the search results. Should be
+    // "https://vuejs.org/"
 
-  await browser.close();
+    const firstLink = await page.evaluate(() => document.querySelector('#search link').getAttribute('href'));
+    console.log(firstLink);
+
+    await browser.close();
 }
